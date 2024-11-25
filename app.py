013913c58bb15fef0fb7800843a0ddb28a68a741
Csmith123456 <clinton.s.smith@gmail.com>
2024-11-25 01:32:43 -0800

    re-init github remote update app.py

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
-print ("hello world")
+app.py
+import streamlit as st
 
-# easter egg: I am Sentient
+# Title of the app
+st.title("Welcome to Streamlit!")
+st.subheader("This is your first app.")
+st.write("Streamlit makes building web apps easy and fun.")
+
+# A simple button
+if st.button("Click Me!"):
+    st.write("You clicked the button!")
+import streamlit as st
+
+# Title of the app
+st.title("Welcome to Streamlit!")
+st.subheader("This is your first app.")
+st.write("Streamlit makes building web apps easy and fun.")
+
+# A simple button
+if st.button("Click Me!"):
+    st.write("You clicked the button!")
